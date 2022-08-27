210f20cedbfd311a2f61f7fdbc3cc52ba675faea
KoyaSaeki <koyasaeki@gmail.com>
2022-08-27 16:18:13 +0900

    update: サーバーからのプッシュ通知を受け取る

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const serviceWorker = navigator.serviceWorker;
 
+/**
+ * サービスワーカーを登録する.
+ */
 const registerServiceWorker = async () => {
   if (!serviceWorker) {
     console.error(`サービスワーカーが存在しません。`);
@@ -13,12 +16,42 @@ const registerServiceWorker = async () => {
   }
 };
 
+/**
+ * PUSH 通知を購読する.
+
+ * @returns サブスクリプション
+ */
+const subscribe = async () => {
+  const worker = await serviceWorker.ready;
+  const applicationServerKey = `BDd3_hVL9fZi9Ybo2UUzA284WG5FZR30_95YeZJsiApwXKpNcF1rRPF3foIiBHXRdJI2Qhumhf6_LFTeZaNndIo`;
+
+  return worker.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey,
+  });
+};
+
 // TODO.
 const requestNotification = async () => {
   await window.Notification.requestPermission();
 };
 
+/**
+ * DOM 読込完了後の処理.
+ */
 document.addEventListener(`DOMContentLoaded`, async () => {
   await requestNotification();
   await registerServiceWorker();
+  const subscription = await subscribe();
+
+  /**
+   * ボタンクリック時にプッシュ通知する.
+   */
+  const btn = document.getElementById("btn");
+  btn.addEventListener(`click`, () => {
+    fetch("https://web-push-server.vercel.app/api/send", {
+      method: "POST",
+      body: JSON.stringify({ subscription, payload: { title: `HELLO WORLD` } }),
+    });
+  });
 });
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
+/**
+ * コンソールの表示をわかりやすくする.
+ */
+const url = `https://unpkg.com/@gauntface/logger@3.0.13/build/browser-globals.js`;
+importScripts(url);
+logger.setPrefix(`service worker`);
+
 const notification = (title, body) => {
   self.registration.showNotification(title, { body, tag: Math.random() });
 };
